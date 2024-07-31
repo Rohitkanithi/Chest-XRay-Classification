459b0ff47eb0abae660c9569ad5cf1729574b6ac
rohitkanithi <rohitkanithi@gmail.com>
2024-07-31 19:43:59 -0400

    data tranasformation process updated

--- Xray/constant/training_pipeline/__init__.py
+++ Xray/constant/training_pipeline/__init__.py
@@ -8,6 +8,23 @@ TIMESTAMP: datetime=datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 ARTIFACT_DIR: str="artifacts"
 BUCKET_NAME: str="lungsxray"
 S3_DATA_FOLDER: str="data"
+
+
+# Data transformation
 CLASS_LABEL_1: str="NORMAL"
 CLASS_LABEL_2: str="PHNEUMONIA"
-
+BRIGHTNESS: int = 0.10
+CONTRAST: int = 0.1
+SATURATION: int = 0.10
+HUE: int = 0.1
+RESIZE: int = 224
+CENTERCROP: int = 224
+RANDOMROTATION: int = 10
+NORMALIZE_LIST_1: List[int] = [0.485, 0.456, 0.406]
+NORMALIZE_LIST_2: List[int] = [0.229, 0.224, 0.225]
+TRAIN_TRANSFORM_KEY: str = "xray_train_transforms"
+TRAIN_TRANSFORM_FILE: str = "train_transforms.pkl"
+TEST_TRANSFORMS_FILE: str = "test_transforms.pkl"
+BATCH_SIZE: int = 2
+SHUFFLE: bool = False
+PIN_MEMORY: bool = True
